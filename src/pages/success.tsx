9d36791275127b957c22e9feddf8ce3160d0ffb3
jonatan timoteo dos santos <jonatan_ts@hotmail.com>
2022-09-21 09:53:05 -0300

    page success

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,22 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
   return (
-    <h2>Sucesso</h2>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Jonatan Timóteo</strong>, sua <strong>modelo camisa</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">
+        Voltar ao Catálago
+      </Link>
+    </SuccessContainer>
   )
 }
