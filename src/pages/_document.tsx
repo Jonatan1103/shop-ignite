142066ad3e9a3d5df1a23b6de15ba52ef54439cc
jonatan timoteo dos santos <jonatan_ts@hotmail.com>
2022-09-21 11:37:25 -0300

    finalized :tadda:

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="shortcut icon" href="/favicon/icon.png"/>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
-
         <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }}/>
       </Head>
       <body>
